eb51fdbb95752889399009cc142a5ccd600212f9
KnorpelSenf <steffen.trog@fastleansmart.com>
2026-04-04 13:50:34 +0200

    feat: `renderToWindow` (#11)

--- examples/jsx.tsx
+++ examples/jsx.tsx
@@ -1,6 +1,5 @@
 /** @jsxImportSource npm:preact@10.29.1 */
-import { renderRawHTML } from "../packages/quox/mod.ts";
-import { render } from "npm:preact-render-to-string@6.6.7";
+import { renderToWindow } from "../packages/quox/mod.ts";
 
 const MyDescription = () => {
   return (
@@ -8,24 +7,20 @@ const MyDescription = () => {
       <h2 style={{ color: "red" }}>
         My description. This is a test of JSX.
       </h2>
-      <p>I'm really surprised that it works.</p>
+      <p>It is so cool that it works!</p>
     </>
   );
 };
 
 function App() {
   return (
-    <html>
-      <body>
-        <h1>JSX/TSX Demo</h1>
-        <MyDescription />
-      </body>
-    </html>
+    <>
+      <h1>JSX/TSX Demo</h1>
+      <MyDescription />
+    </>
   );
 }
 
 if (import.meta.main) {
-  const html = `<!DOCTYPE html>${render(<App />)}`;
-
-  await renderRawHTML(html);
+  await renderToWindow(<App />);
 }
